4d43346ebf071f1f7fe2f7e02de83e181812abb4
Daniel Kovacs <danielk9538@hstat.org>
2023-03-09 06:41:24 +0000

    created destination and layout

---
+++ script.js
@@ -0,0 +1,9 @@
+let ownerName = "BHP"; // update the part BETWEEN the "quotes"
+let userName = "danielk9538"; // same here
+
+document.querySelectorAll(".owner-name").forEach((e) => {
+    e.innerHTML = ownerName;
+});
+
+document.querySelector("#github").href = "http://github.com/" + userName;
+document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
